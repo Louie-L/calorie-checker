ab8b2e67dc2ea2e5001fe2297cafa2fc24db9078
Louie <yiu10684755@gmail.com>
2021-06-28 18:38:15 +0800

    feat: add component props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text='Title 1' />
+      <Todo text='Title 2' />
+      <Todo text='Title 3' />
     </div>
   );
 }
