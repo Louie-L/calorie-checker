4bc7ef669c7a3266d1bf706b91cf01b8e6c69f6e
Louie <yiu10684755@gmail.com>
2021-06-28 22:22:58 +0800

    feat: wrapper components props children

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import { Route, Switch } from 'react-router-dom';
 import AllCaloriesPage from './pages/AllCalories';
 import NewCaloriePage from './pages/NewCalorie';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>      
-      <MainNavigation />
-      <Switch>
-        <Route path='/' exact>
-          <AllCaloriesPage />
-        </Route>
-        <Route path='/new-calorie'>
-          <NewCaloriePage />
-        </Route>
-        <Route path='/favorites'>
-          <FavoritesPage />
-        </Route>
-      </Switch>
+    <div>
+      <Layout>
+        <Switch>
+          <Route path='/' exact>
+            <AllCaloriesPage />
+          </Route>
+          <Route path='/new-calorie'>
+            <NewCaloriePage />
+          </Route>
+          <Route path='/favorites'>
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
--- src/components/calories/CalorieItem.js
+++ src/components/calories/CalorieItem.js
@@ -1,18 +1,21 @@
+import Card from '../ui/Card';
 import classes from './CalorieItem.module.css';
 
 function CalorieItem(props) {
   return (
     <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favorites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
