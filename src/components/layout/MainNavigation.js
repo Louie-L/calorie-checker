e774ace5c7fd7580a2fb814473b187a940ccc753
Louie <yiu10684755@gmail.com>
2021-06-28 21:32:05 +0800

    feat: styling with css modules

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 
+import classes from './MainNavigation.module.css';
+
 function MainNavigation() {
   return (
-    <header>
-      <div>Calorie Checker</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>Calorie Checker</div>
       <nav>
         <ul>
           <li>
