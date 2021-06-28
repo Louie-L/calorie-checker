71e3b5fc1ce32dc0dcfabedd4baa958849be002b
Louie <yiu10684755@gmail.com>
2021-06-28 21:04:56 +0800

    feat: clean up project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import Todo from './components/Todo'
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text='Title 1' />
-      <Todo text='Title 2' />
-      <Todo text='Title 3' />
+      
     </div>
   );
 }
