c9a57c159feb4b5b7480905a20144682145e3c5b
Louie <yiu10684755@gmail.com>
2021-06-28 22:55:09 +0800

    feat: get user input on form submit

--- src/components/calories/NewCalorieForm.js
+++ src/components/calories/NewCalorieForm.js
@@ -1,11 +1,27 @@
+import { useRef } from 'react';
+
 import Card from '../ui/Card';
 import classes from './NewCalorieForm.module.css';
 
 function NewCalorieForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const descriptionInputRef = useRef();
+
   function submitHandler(event) {
     event.preventDefault();
 
-    
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const calorieData = {
+      title: enteredTitle,
+      image: enteredImage,
+      description: enteredDescription,
+    };
+
+    console.log(calorieData);
   }
 
   return (
@@ -13,15 +29,20 @@ function NewCalorieForm() {
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='title'>Calorie Title</label>
-          <input type='text' required id='title' />
+          <input type='text' required id='title' ref={titleInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='image'>Calorie Image</label>
-          <input type='url' required id='image' />
+          <input type='url' required id='image' ref={imageInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='description'>Description</label>
-          <textarea id='description' required rows='5'></textarea>
+          <textarea
+            id='description'
+            required
+            rows='5'
+            ref={descriptionInputRef}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Calorie</button>
