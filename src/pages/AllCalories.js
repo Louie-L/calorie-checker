107e4b9607ae0dc096dcd93314c63b6bf70061f1
Louie <yiu10684755@gmail.com>
2021-06-29 15:08:00 +0800

    refactor: styling

--- src/pages/AllCalories.js
+++ src/pages/AllCalories.js
@@ -4,7 +4,7 @@ function AllCaloriesPage() {
   return (
     <section>
       <h1>所有分類</h1>
-      <ol>
+      <ul>
         <li><Link to='/food'>食品</Link></li>
         <li><Link to='/grains'>五穀類</Link></li>
         <li><Link to='/fisheggmeat'>魚、蛋、肉類</Link></li>
@@ -14,7 +14,7 @@ function AllCaloriesPage() {
         <li><Link to='/fruits'>水果類</Link></li>
         <li><Link to='/drinks'>飲品類</Link></li>
         <li><Link to='/others'>其他</Link></li>
-      </ol>
+      </ul>
     </section>
   );
 }
