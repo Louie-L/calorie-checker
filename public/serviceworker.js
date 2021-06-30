f761004390ae6b1e9685209af513f86b0f1e144c
Louie <yiu10684755@gmail.com>
2021-06-30 16:06:07 +0800

    feat: add serviceworker

---
+++ public/serviceworker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = [ '/', '/index.html' ];
+
+
+// Install SW
+self.addEventListener('install', (event) => {
+    console.log('Serviceworker Installed');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+    console.log('Serviceworker Activated');
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+            
+    )
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(res) {
+                if (res) {
+                    return res;
+                } else {
+                    return fetch(event.request);
+                }
+            })
+    )
+});
