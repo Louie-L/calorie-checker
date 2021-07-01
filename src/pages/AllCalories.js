c6b3f541126ccc769ac8965d3c7393b8eca74c0b
Louie <yiu10684755@gmail.com>
2021-07-01 23:15:25 +0800

    feat: styling

--- src/pages/AllCalories.js
+++ src/pages/AllCalories.js
@@ -6,20 +6,20 @@ import RedTag from '../components/ui/RedTag'
 function AllCaloriesPage() {
   return (
     <section>
-      <ListGroup className="m-4">
-        <ListGroup.Item><Link to='/bmi'>BMI</Link><RedTag text='New!'/><hr/></ListGroup.Item>
-        <ListGroup.Item><Link to='/calorie-calculate'>消耗和所需熱量</Link><RedTag text='New!' /><hr/></ListGroup.Item>
-        <ListGroup.Item><Link to='/calorie-table'>卡路里表</Link><RedTag text='Updated!' /><hr/></ListGroup.Item>
+      <ListGroup className="m-4 p-2">
+        <ListGroup.Item action className="border-bottom-0"><Link to='/bmi'>BMI</Link><RedTag text='New!'/><hr/></ListGroup.Item>
+        <ListGroup.Item action className="border-bottom-0"><Link to='/calorie-calculate'>消耗和所需熱量</Link><RedTag text='New!' /><hr/></ListGroup.Item>
+        <ListGroup.Item action className="border-bottom-0"><Link to='/calorie-table'>卡路里表</Link><RedTag text='Updated!' /><hr/></ListGroup.Item>
 
-        <ListGroup.Item><Link to='/food'>食品</Link></ListGroup.Item>
-        <ListGroup.Item><Link to='/grains'>五穀類</Link></ListGroup.Item>
-        <ListGroup.Item><Link to='/fisheggmeat'>魚、蛋、肉類</Link></ListGroup.Item>
-        <ListGroup.Item><Link to='/seafood'>海鮮類</Link></ListGroup.Item>
-        <ListGroup.Item><Link to='/vegetables'>蔬菜類</Link></ListGroup.Item>
-        <ListGroup.Item><Link to='/desserts'>甜品類</Link></ListGroup.Item>
-        <ListGroup.Item><Link to='/fruits'>水果類</Link></ListGroup.Item>
-        <ListGroup.Item><Link to='/drinks'>飲品類</Link></ListGroup.Item>
-        <ListGroup.Item><Link to='/others'>其他</Link></ListGroup.Item>
+        <ListGroup.Item action className="border-bottom-0"><Link to='/food'>食品</Link></ListGroup.Item>
+        <ListGroup.Item action className="border-bottom-0"><Link to='/grains'>五穀類</Link></ListGroup.Item>
+        <ListGroup.Item action className="border-bottom-0"><Link to='/fisheggmeat'>魚、蛋、肉類</Link></ListGroup.Item>
+        <ListGroup.Item action className="border-bottom-0"><Link to='/seafood'>海鮮類</Link></ListGroup.Item>
+        <ListGroup.Item action className="border-bottom-0"><Link to='/vegetables'>蔬菜類</Link></ListGroup.Item>
+        <ListGroup.Item action className="border-bottom-0"><Link to='/desserts'>甜品類</Link></ListGroup.Item>
+        <ListGroup.Item action className="border-bottom-0"><Link to='/fruits'>水果類</Link></ListGroup.Item>
+        <ListGroup.Item action className="border-bottom-0"><Link to='/drinks'>飲品類</Link></ListGroup.Item>
+        <ListGroup.Item action className="border-bottom-0"><Link to='/others'>其他</Link></ListGroup.Item>
       </ListGroup>
     </section>
   );
